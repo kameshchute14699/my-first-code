a520dd33514cc229423c77197f5207ba27560d71
kameshchute14699 <153077838+kameshchute14699@users.noreply.github.com>
2023-12-06 16:22:12 +0530

    i have imported kamesh fgfff.bat
    i have taken a kamesh fgfff.bat file where i have entered

---
+++ kamesh fgfff.bat
@@ -0,0 +1,20 @@
+<html>
+<head>
+<title><b><u>THe kedarnath temple.</u></b></title>
+</head>
+<body>
+<h1>The kedarnath temple</h1>
+<p><b>Kedarnath</b>is the most remote of the four <b>Chota Char Dham</b>pilgrimage sites.</p>
+<p>It is located in the <i>Himalayas,</i> about <del>3,583</del>m (111,755ft) above <b> sea level</b> near the chorabari glacier, which is the source of the <b>Mandakini river.</b></p>
+<br>
+<ul>
+<i>Mandakini river.</i>
+<h1>the<strong> "krdarnath" means "the Lord of the filed."</strong> It was derived from the <b>sanskruti</b></h1>
+<h2><del>Prachina-Shankara-Vijaya, state that he died at <b>Kanchipuram.</b></h2>
+<h3><del>The ruins of a mounment marking the purpoter resting place of adi shankaracharya are located at Kedarnath.</del></h3>
+<h4>water formula: H<sub>2</sub>o</p>.</h4>
+<h5>2<sup>2</sup></p>.</h5>
+<h6>thanks.</h6>
+
+</body>
+</html>
